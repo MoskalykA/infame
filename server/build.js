fc3dc4b9a5149ade4095b4ab09089a943e29797c
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-03-03 23:53:22 +0100

    build: new configuration for bundles

--- server/build.js
+++ server/build.js
@@ -7,17 +7,20 @@ build({
   inject: [
     "./src/env.ts",
     "./src/infame/events/playerConnecting.ts",
-    "./src/infame/events/playerJoining.ts",
+    "./src/infame/nets/playerConnected.ts",
+    "./src/infame/nets/characters/createCharacter.ts",
     "./src/infame/nets/characters/selectCharacter.ts",
     "./src/infame/utils/characters/selectCharacter.ts",
-    "./src/infame/utils/characters/characterData.ts",
     "./src/infame/utils/addNotification.ts",
     "./src/infame/utils/sql.ts",
+    "./src/infame/utils/sql/idToString.ts",
+    "./src/infame/types/characterData.ts",
     "./src/infame/types/notificationType.ts",
   ],
   outfile: "build/bundle.js",
   target: ["node16"],
   platform: "node",
+  external: ["mongodb"],
   plugins: [filelocPlugin()],
 })
   .then(() => console.log("🚀 [server]: Built successfully!"))
