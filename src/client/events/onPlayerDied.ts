01030c4c269bd086c9d456529a25b821da565ea6
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-03-25 19:44:57 +0100

    refactor: delete the kill command

--- src/client/events/onPlayerDied.ts
+++ src/client/events/onPlayerDied.ts
@@ -1,11 +1,3 @@
 import { onDeath } from "client/events/players/onDeath";
 
 on("baseevents:onPlayerDied", onDeath);
-
-RegisterCommand(
-  "kill",
-  () => {
-    SetEntityHealth(GetPlayerPed(-1), 0);
-  },
-  false
-);
