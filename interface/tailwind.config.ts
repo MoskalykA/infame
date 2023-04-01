8fd5eecb154091b247711fdd0f0c2cde391965d2
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-04-01 11:23:35 +0200

    refactor: tailwindcss update

--- interface/tailwind.config.ts
+++ interface/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -9,4 +10,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+} satisfies Config;
