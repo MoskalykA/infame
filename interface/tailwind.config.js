74af2aab6db76e39b86bad8ed00b65a84fa8c0e5
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-05-01 21:52:53 +0200

    refactor: back to tailwindjs

--- interface/tailwind.config.js
+++ interface/tailwind.config.js
@@ -1,5 +1,4 @@
-import type { Config } from "tailwindcss";
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -10,4 +9,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
